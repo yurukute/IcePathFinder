5bf2ada0f35b7a4a85da3900869b26b1c731e310
yurukute <nguyenkhanhdung.9999@gamil.com>
2022-10-17 03:05:32 +0700

    generalize dialogs

--- mywidgets.py
+++ mywidgets.py
@@ -1,10 +1,11 @@
-from PySide6.QtWidgets import (QDialog, QDialogButtonBox, QFormLayout,
-                               QGroupBox, QLabel, QSpinBox, QVBoxLayout, QWidget, QPushButton)
+from PySide6.QtWidgets import (QColorDialog, QDialog, QDialogButtonBox,
+                               QFormLayout, QGroupBox, QLabel, QPushButton,
+                               QSpinBox, QVBoxLayout)
 
 
-class NewMazeDialog(QDialog):
+class MyDialog(QDialog):
     def __init__(self):
-        super(NewMazeDialog, self).__init__()
+        super(MyDialog, self).__init__()
         self.create_form()
 
         btns = QDialogButtonBox(QDialogButtonBox.Ok | QDialogButtonBox.Cancel)
@@ -15,6 +16,17 @@ class NewMazeDialog(QDialog):
         mainLayout.addWidget(self.form)
         mainLayout.addWidget(btns)
         self.setLayout(mainLayout)
+
+    def create_form(self):
+        return
+
+    def values(self):
+        return
+
+
+class NewMazeDialog(MyDialog):
+    def __init__(self):
+        super(NewMazeDialog, self).__init__()
         self.setWindowTitle("Create New Maze")
 
     def create_form(self):
@@ -52,3 +64,43 @@ class NewMazeDialog(QDialog):
         rocks = self.rock_amt.value()
         snows = self.snow_amt.value()
         return [row, col, rocks, snows]
+
+
+class PickColorDialog(NewMazeDialog):
+    def __init__(self, bfs_color, dfs_color):
+        self.bfs_color = bfs_color
+        self.dfs_color = dfs_color
+
+        super(PickColorDialog, self).__init__()
+        self.setWindowTitle("Change path's color")
+
+    def create_form(self):
+        self.form = QGroupBox("Pick color for paths:")
+        layout = QFormLayout()
+        self.bfs_button = QPushButton()
+        self.dfs_button = QPushButton()
+
+        self.bfs_button.setPalette(self.bfs_color)
+        self.dfs_button.setPalette(self.dfs_color)
+
+        self.bfs_button.clicked.connect(self.bfs_button_clicked)
+        self.dfs_button.clicked.connect(self.dfs_button_clicked)
+
+        layout.addRow(QLabel("BFS:"), self.bfs_button)
+        layout.addRow(QLabel("DFS:"), self.dfs_button)
+        self.form.setLayout(layout)
+
+    def bfs_button_clicked(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.bfs_color = color
+            self.bfs_button.setPalette(color)
+
+    def dfs_button_clicked(self):
+        color = QColorDialog.getColor()
+        if color.isValid():
+            self.dfs_color = color
+            self.dfs_button.setPalette(color)
+
+    def values(self):
+        return [self.bfs_color, self.dfs_color]
