d7d482204dd239da28d221b8287748bcc8fb7862
yurukute <nguyenkhanhdung.9999@gamil.com>
2022-10-17 03:06:57 +0700

    add translation files

---
+++ translate/vi_VN.ts
@@ -0,0 +1,123 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="vi_VN" sourcelanguage="en_US">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../mainwindow.py" line="21"/>
+        <source>Welcome to Ice Path Finder</source>
+        <translation>Chào mừng đến với Ice Path Finder</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="28"/>
+        <source>&amp;New</source>
+        <translation>&amp;Tạo mới</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="29"/>
+        <source>&amp;Open...</source>
+        <translation>&amp;Mở...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="30"/>
+        <source>&amp;Quit</source>
+        <translation>&amp;Thoát</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="40"/>
+        <source>&amp;File</source>
+        <translation>&amp;Tệp</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="47"/>
+        <source>&amp;Change path&apos;s color</source>
+        <translation>Thay đổi &amp;màu đường đi</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="48"/>
+        <source>&amp;English</source>
+        <translation>Tiếng &amp;Anh</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="49"/>
+        <source>&amp;Vietnamese</source>
+        <translation>Tiếng &amp;Việt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="55"/>
+        <source>&amp;Option</source>
+        <translation>&amp;Cài đặt</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="58"/>
+        <source>Change &amp;language</source>
+        <translation>Thay đổi &amp;ngôn ngữ</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="63"/>
+        <source>&amp;Help...</source>
+        <translation>&amp;Trợ giúp...</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="64"/>
+        <source>&amp;About Ice Path Finder</source>
+        <translation>&amp;Về Ice Path Finder</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="85"/>
+        <source>Import maze from file</source>
+        <translation>Thêm mê cung từ tập tin</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="87"/>
+        <source>Solve</source>
+        <translation>Giải</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="186"/>
+        <source>Solved in %s secconds</source>
+        <translation>Đã giải trong %s giây</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="190"/>
+        <source>About - Ice Path Finder</source>
+        <translation></translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="71"/>
+        <source>&amp;Help</source>
+        <translation>&amp;Trợ giúp</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="122"/>
+        <source>Error</source>
+        <translation>Lỗi</translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="122"/>
+        <source>
+          CANNOT READ MAZE
+          Plese make sure the the file is written in the appropriate format:
+          1. The last line does not contain newline character.
+          2. Other lines have the same length.
+        </source>
+        <translation>
+          KHÔNG THỂ ĐỌC MÊ CUNG
+          Hãy đảm bảo rằng tập tin được viết theo đúng định dạng:
+          1. Dòng cuối cùng không bao gồm ký tự xuống hàng.
+          2. Các dòng còn lại có cùng chiều dài.
+        </translation>
+    </message>
+    <message>
+        <location filename="../mainwindow.py" line="190"/>
+        <source>College Project - Basic Topics:
+Apply blind search algorithms in solving ice maze
+Develop by Nguyen Khanh Dung
+Version: 1.0</source>
+        <translation>Niên luận cơ sở ngành:
+Áp dụng các giải thuật tìm kiếm để tìm đường đi trong mê cung băng
+Thực hiện bởi: Nguyễn Khánh Dung
+Phiên bản: 1.0</translation>
+    </message>
+</context>
+</TS>
