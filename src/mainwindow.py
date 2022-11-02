ae21a8863f353cae6a74a5a1a1b2a34f9cf41431
yurukute <nguyenkhanhdung.9999@gamil.com>
2022-11-02 10:44:20 +0700

    use MyMenuBar widget

--- src/mainwindow.py
+++ src/mainwindow.py
@@ -1,10 +1,9 @@
 import time
-from PySide6.QtGui import QAction
 from PySide6.QtCore import QSize, Qt, QTranslator, QEvent
 from PySide6.QtWidgets import (QApplication, QFileDialog, QInputDialog, QLabel,
                                QMainWindow, QMessageBox)
 
-from mywidgets import NewMazeDialog, PickColorDialog, MyAppView
+from mywidgets import NewMazeDialog, PickColorDialog, MyMenuBar, MyAppView
 from icemaze import IceMaze
 
 
@@ -14,7 +13,7 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle('Ice Path Finder')
         self.setFixedSize(QSize(800, 600))
-        self.init_menubar()
+        self.setMenuBar(self.get_menubar())
 
         self.appview = MyAppView()
 
@@ -31,64 +30,23 @@ class MainWindow(QMainWindow):
         self.msg = QLabel(self.tr('Welcome to Ice Path Finder'))
         self.statusBar().addPermanentWidget(self.msg)
 
-    def init_menubar(self):
-        menu = self.menuBar()
+    def get_menubar(self):
+        menu = MyMenuBar()
 
-        # File menu
-        new_action = QAction(self.tr('&New'), self)
-        load_action = QAction(self.tr('&Open...'), self)
-        quit_action = QAction(self.tr('&Quit'), self)
+        menu.new_action.triggered.connect(self.init_maze)
+        menu.load_action.triggered.connect(self.load_maze)
+        menu.quit_action.triggered.connect(self.close)
+        menu.change_color_action.triggered.connect(self.change_color)
+        menu.english_action.triggered.connect(self.change_language)
+        menu.vietnamese_action.triggered.connect(self.change_language)
 
-        new_action.setShortcut('Ctrl+N')
-        load_action.setShortcut('Ctrl+O')
-        quit_action.setShortcut('Ctrl+Q')
-
-        new_action.triggered.connect(self.init_maze)
-        load_action.triggered.connect(self.load_maze)
-        quit_action.triggered.connect(self.close)
-
-        file_menu = menu.addMenu(self.tr('&File'))
-        file_menu.addAction(new_action)
-        file_menu.addAction(load_action)
-        file_menu.addSeparator()
-        file_menu.addAction(quit_action)
-
-        # Option menu
-        change_color_action = QAction(self.tr('&Change path\'s color'), self)
-        english_action = QAction(self.tr('&English'), self)
-        vietnamese_action = QAction(self.tr('&Vietnamese'), self)
-
-        change_color_action.triggered.connect(self.change_color)
-        english_action.triggered.connect(self.change_language)
-        vietnamese_action.triggered.connect(self.change_language)
-        vietnamese_action.setData('vi_VN')
-
-        option_menu = menu.addMenu(self.tr('&Option'))
-        option_menu.addAction(change_color_action)
-
-        change_language_menu = option_menu.addMenu(self.tr('Change &language'))
-        change_language_menu.addAction(english_action)
-        change_language_menu.addAction(vietnamese_action)
-
-        # Help menu
-        help_action = QAction(self.tr('&Help...'), self)
-        about_action = QAction(self.tr('&About Ice Path Finder'), self)
-
-        help_action.setShortcut('F1')
-
-        help_action.triggered.connect(self.show_help)
-        about_action.triggered.connect(self.show_about)
-
-        help_menu = menu.addMenu(self.tr('&Help'))
-        help_menu.addAction(help_action)
-        help_menu.addSeparator()
-        help_menu.addAction(about_action)
+        return menu
 
     def init_maze(self):
         dialog = NewMazeDialog()
         if dialog.exec():
             self.maze = IceMaze(*dialog.values())
-            self.appview.draw_maze()
+            self.appview.draw_maze(self.maze.get_map())
 
     def load_maze(self):
         dialog = QFileDialog()
@@ -125,21 +83,10 @@ class MainWindow(QMainWindow):
                 self.tr('Solved in ') + ('%s' % (time.time() - start_time)) +
                 self.tr(' seconds.'))
 
-    def show_about(self):
-        QMessageBox.information(
-            self, self.tr('About - Ice Path Finder'),
-            self.tr('College Project - Basic Topics:\n'
-                    'Apply blind search algorithms in solving ice maze\n'
-                    'Develop by Nguyen Khanh Dung\n'
-                    'Version: 1.0'))
-
-    def show_help(self):
-        print('help clicked')
-
     def change_language(self):
         lang = self.sender().data()
         if lang:
-            self.translator.load(f'translate/{lang}')
+            self.translator.load(f'../translate/{lang}')
             QApplication.instance().installTranslator(self.translator)
         else:
             QApplication.instance().removeTranslator(self.translator)
@@ -150,8 +97,7 @@ class MainWindow(QMainWindow):
         super(MainWindow, self).changeEvent(event)
 
     def retranslateUi(self):
-        self.menuBar().clear()
-        self.init_menubar()
+        self.setMenuBar(self.get_menubar())
         self.appview.reset_buttons()
         self.msg.setText(self.tr('Welcome to Ice Path Finder'))
 
