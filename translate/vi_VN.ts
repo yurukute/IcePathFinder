85eb638c7cf6affefb6fdeaed6c0c15d7663345a
yurukute <nguyenkhanhdung.9999@gamil.com>
2022-11-04 22:01:49 +0700

    update

--- translate/vi_VN.ts
+++ translate/vi_VN.ts
@@ -4,44 +4,39 @@
 <context>
     <name>MainWindow</name>
     <message>
-        <location filename="../src/mainwindow.py" line="103"/>
+        <location filename="../src/mainwindow.py" line="99"/>
         <source>Welcome to Ice Path Finder</source>
         <translation>Chào mừng đến với Ice Path Finder</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.py" line="60"/>
+        <location filename="../src/mainwindow.py" line="57"/>
         <source>Error</source>
         <translation>Lỗi</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.py" line="60"/>
+        <location filename="../src/mainwindow.py" line="57"/>
         <source>CANNOT READ MAZE
 File is empty.</source>
         <translation>KHÔNG THỂ ĐỌC MÊ CUNG
 Tập tin trống.</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.py" line="72"/>
+        <location filename="../src/mainwindow.py" line="69"/>
         <source>Solve maze</source>
         <translation>Giải mê cung</translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.py" line="72"/>
+        <location filename="../src/mainwindow.py" line="69"/>
         <source>Choose algorithm:</source>
         <translation>Chọn giải thuật</translation>
     </message>
     <message>
-        <location filename="../mainwindow.py" line="129"/>
-        <source>About - Ice Path Finder</source>
-        <translation type="obsolete">Thông tin - Ice Path Finder</translation>
-    </message>
-    <message>
-        <location filename="../src/mainwindow.py" line="82"/>
+        <location filename="../src/mainwindow.py" line="79"/>
         <source>Solved in </source>
         <translation>Đã giải trong </translation>
     </message>
     <message>
-        <location filename="../src/mainwindow.py" line="82"/>
+        <location filename="../src/mainwindow.py" line="79"/>
         <source> seconds.</source>
         <translation> giây.</translation>
     </message>
@@ -49,22 +44,22 @@ Tập tin trống.</translation>
 <context>
     <name>MyAppView</name>
     <message>
-        <location filename="../src/mywidgets.py" line="202"/>
+        <location filename="../src/mywidgets.py" line="226"/>
         <source>Import maze from file</source>
         <translation>Thêm mê cung từ tập tin</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="203"/>
+        <location filename="../src/mywidgets.py" line="227"/>
         <source>Solve</source>
         <translation>Giải</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="234"/>
+        <location filename="../src/mywidgets.py" line="259"/>
         <source>Notification</source>
         <translation>Thông báo</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="234"/>
+        <location filename="../src/mywidgets.py" line="259"/>
         <source>There is no solution for this maze.</source>
         <translation>Không tồn tại lời giải cho mê cung này.</translation>
     </message>
@@ -87,57 +82,57 @@ Tập tin trống.</translation>
         <translation>&amp;Thoát</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="25"/>
+        <location filename="../src/mywidgets.py" line="27"/>
         <source>&amp;File</source>
         <translation>&amp;Tệp</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="32"/>
+        <location filename="../src/mywidgets.py" line="34"/>
         <source>&amp;Change path&apos;s color</source>
         <translation>Thay đổi &amp;màu đường đi</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="34"/>
+        <location filename="../src/mywidgets.py" line="36"/>
         <source>&amp;English</source>
         <translation>Tiếng &amp;Anh</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="35"/>
+        <location filename="../src/mywidgets.py" line="37"/>
         <source>&amp;Vietnamese</source>
         <translation>Tiếng &amp;Việt</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="38"/>
+        <location filename="../src/mywidgets.py" line="40"/>
         <source>&amp;Option</source>
         <translation>&amp;Cài đặt</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="41"/>
+        <location filename="../src/mywidgets.py" line="43"/>
         <source>Change &amp;language</source>
         <translation>Thay đổi &amp;ngôn ngữ</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="46"/>
+        <location filename="../src/mywidgets.py" line="48"/>
         <source>&amp;Help...</source>
         <translation>&amp;Trợ giúp...</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="47"/>
+        <location filename="../src/mywidgets.py" line="49"/>
         <source>&amp;About Ice Path Finder</source>
         <translation>&amp;Về Ice Path Finder</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="54"/>
+        <location filename="../src/mywidgets.py" line="56"/>
         <source>&amp;Help</source>
         <translation>&amp;Trợ giúp</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="63"/>
+        <location filename="../src/mywidgets.py" line="65"/>
         <source>About - Ice Path Finder</source>
         <translation>Thông tin - Ice Path Finder</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="63"/>
+        <location filename="../src/mywidgets.py" line="65"/>
         <source>College Project - Basic Topics:
 Apply blind search algorithms in solving ice maze
 Develop by Nguyen Khanh Dung
@@ -152,32 +147,32 @@ Phiên bản: 1.0
 <context>
     <name>NewMazeDialog</name>
     <message>
-        <location filename="../src/mywidgets.py" line="97"/>
+        <location filename="../src/mywidgets.py" line="111"/>
         <source>Create New Maze</source>
         <translation>Tạo Mê Cung Mới</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="100"/>
+        <location filename="../src/mywidgets.py" line="114"/>
         <source>Insert amount of</source>
         <translation>Nhập vào giá trị của:</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="115"/>
+        <location filename="../src/mywidgets.py" line="129"/>
         <source>Rows:</source>
         <translation>Số hàng:</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="116"/>
+        <location filename="../src/mywidgets.py" line="130"/>
         <source>Columns:</source>
         <translation>Số cột:</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="117"/>
+        <location filename="../src/mywidgets.py" line="131"/>
         <source>Rocks:</source>
         <translation>Số lượng đá:</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="118"/>
+        <location filename="../src/mywidgets.py" line="132"/>
         <source>Snows:</source>
         <translation>Số lượng tuyết:</translation>
     </message>
@@ -185,12 +180,12 @@ Phiên bản: 1.0
 <context>
     <name>PickColorDialog</name>
     <message>
-        <location filename="../src/mywidgets.py" line="143"/>
+        <location filename="../src/mywidgets.py" line="156"/>
         <source>Change path&apos;s color</source>
         <translation>Thay đổi màu đường đi</translation>
     </message>
     <message>
-        <location filename="../src/mywidgets.py" line="146"/>
+        <location filename="../src/mywidgets.py" line="159"/>
         <source>Pick color for paths:</source>
         <translation>Chọn màu cho đường đi</translation>
     </message>
