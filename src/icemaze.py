a23d8dd757c3190e2c48f2df1251bdbd869be798
yurukute <nguyenkhanhdung.9999@gamil.com>
2022-11-12 21:33:18 +0700

    reformat and handling missing spaces files

--- src/icemaze.py
+++ src/icemaze.py
@@ -3,51 +3,57 @@ import queue
 
 
 class IceMaze:
+
     def __init__(self, row=0, col=0, rock_num=0, snow_num=0):
         self.__row = row
         self.__col = col
-        self.__map = [[' ']*col for i in range(row)]
-        pos = [i for i in range(row*col)]
+        self.__map = [[' '] * col for i in range(row)]
+        pos = [i for i in range(row * col)]
         rocks = sample(pos, rock_num)
         snows = sample([i for i in pos if i not in rocks], snow_num)
         for i in rocks:
             self.__map[i // col][i % col] = '#'
         for i in snows:
             self.__map[i // col][i % col] = 'x'
-        [start, end] = sample([i for i in pos if
-                               i not in rocks and
-                               i not in snows], 2)
+        [start,
+         end] = sample([i for i in pos if i not in rocks and i not in snows],
+                       2)
         self.__map[start // col][start % col] = 'S'
         self.__map[end // col][end % col] = 'E'
         self.__start = start
+        self.__end = end
 
     def read_maze(strmap):
-        if strmap[len(strmap)-1] == '\n':
+        if strmap[len(strmap) - 1] == '\n':
             strmap = strmap[:-1]
         col = max(len(i) for i in strmap.split('\n'))
         row = len(strmap.split('\n'))
         maze = IceMaze(row, col)
+        maze.__map[maze.__start // col][maze.__start % col] = ' '
+        maze.__map[maze.__end // col][maze.__end % col] = ' '
         i, j = 0, 0
         for c in strmap:
             if c == 'S':
-                maze.__start = i*row + j
+                maze.__start = i * row + j
+            if c == 'E':
+                maze.__end = i * row + j
             if c != '\n':
                 maze.__map[i][j] = c
                 j += 1
             else:
-                i, j = i+1, 0
+                i, j = i + 1, 0
         return maze
 
     def next_tile(self, curr, DIR):
         next = curr
-        while(self.__map[curr // self.__col][curr % self.__col] != 'E'):
+        while (self.__map[curr // self.__col][curr % self.__col] != 'E'):
             if DIR == 'UP' and curr >= self.__col:
                 next = curr - self.__col
-            elif DIR == 'DOWN' and curr < (self.__row - 1)*self.__col:
+            elif DIR == 'DOWN' and curr < (self.__row - 1) * self.__col:
                 next = curr + self.__col
             elif DIR == 'LEFT' and curr % self.__col != 0:
                 next = curr - 1
-            elif DIR == 'RIGHT' and (curr+1) % self.__col != 0:
+            elif DIR == 'RIGHT' and (curr + 1) % self.__col != 0:
                 next = curr + 1
             else:
                 break
@@ -71,42 +77,42 @@ class IceMaze:
 
     def bfs(self):
         result = []
-        mark = [0]*self.__row*(self.__col+1)
-        parent = [-1]*self.__row*(self.__col+1)
+        mark = [0] * self.__row * (self.__col + 1)
+        parent = [-1] * self.__row * (self.__col + 1)
         Queue = queue.Queue()
         Queue.put(self.__start)
         col = self.__col
         while not Queue.empty():
             curr = Queue.get()
-            if(self.__map[curr // col][curr % col] == 'E'):
+            if (self.__map[curr // col][curr % col] == 'E'):
                 result = self.update_result(result, parent, curr)
                 continue
             mark[curr] = 1
             for DIR in ['UP', 'DOWN', 'LEFT', 'RIGHT']:
                 next = self.next_tile(curr, DIR)
-                if (next != curr and next != parent[curr] and
-                        not mark[next] and parent[next] == -1):
+                if (next != curr and next != parent[curr] and not mark[next]
+                        and parent[next] == -1):
                     parent[next] = curr
                     Queue.put(next)
         return result
 
     def dfs(self):
         result = []
-        mark = [0]*self.__row*(self.__col+1)
-        parent = [-1]*self.__row*(self.__col+1)
+        mark = [0] * self.__row * (self.__col + 1)
+        parent = [-1] * self.__row * (self.__col + 1)
         Stack = queue.LifoQueue()
         Stack.put(self.__start)
         col = self.__col
         while not Stack.empty():
             curr = Stack.get()
-            if(self.__map[curr // col][curr % col] == 'E'):
+            if (self.__map[curr // col][curr % col] == 'E'):
                 result = self.update_result(result, parent, curr)
                 continue
             mark[curr] = 1
             for DIR in reversed(['UP', 'DOWN', 'LEFT', 'RIGHT']):
                 next = self.next_tile(curr, DIR)
-                if (next != curr and next != parent[curr] and
-                        not mark[next] and parent[next] == -1):
+                if (next != curr and next != parent[curr] and not mark[next]
+                        and parent[next] == -1):
                     parent[next] = curr
                     Stack.put(next)
         return result
